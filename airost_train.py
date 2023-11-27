37cea71c2d296a6749c12e0db92052c2075ca649
Bryan <111334322+notEhEnG@users.noreply.github.com>
2023-11-27 21:22:23 +0800

    Update airost_train.py
    Editing details

--- airost_train.py
+++ airost_train.py
@@ -2,9 +2,9 @@ from ultralytics import YOLO
 
 #Loading a model
 model = YOLO("weights/yolov8n.pt")
-model = YOLO("yolov8n.yaml") #Build a new model from scratch
-# model = YOLO("weights/yolov8n.pt")
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')
+# model = YOLO("yolov8n.yaml") # Build a new model (Optional)
+# model = YOLO('yolov8n.yaml').load('yolov8n.pt')
 
 #Training the models mentioned above
+# config.yaml is where the images you want the model to be trained. 
 results = model.train(data = "config.yaml", epochs = 10)
